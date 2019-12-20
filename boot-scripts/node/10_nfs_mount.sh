c37ccf11732c17218b15caa54adb5e834de5b367
Londenberg, Kai (K-FIL/13) <kai.londenberg@volkswagen.de>
2019-12-20 14:15:08 +0100

    Small fixes

--- boot-scripts/node/10_nfs_mount.sh
+++ boot-scripts/node/10_nfs_mount.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 echo "Attemping to mount /nfs from master at mountpoint /nfs"
-mount 10.23.23.10:/nfs /nfs
+mount 10.23.23.10:/data/nfs /data/nfs
--- setup-scripts/node/10_nfs_mount.sh
+++ setup-scripts/node/10_nfs_mount.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-echo "Attemping to mount /nfs from master at mountpoint /nfs"
+echo "Attemping to mount /data/nfs from master at mountpoint /data/nfs"
 sleep 8
-mkdir -p /nfs
-chmod 777 /nfs
-mount 10.23.23.10:/nfs /nfs
+mkdir -p /data/nfs
+chmod 777 /data/nfs
+mount 10.23.23.10:/data/nfs /data/nfs
